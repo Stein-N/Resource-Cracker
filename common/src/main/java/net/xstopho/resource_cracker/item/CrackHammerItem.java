850701f98e73cf1b7db42c560ecbb13b2a77585c
Stein-N <nico-stein-98@outlook.de>
2024-01-24 20:47:25 +0100

    fixing loader incompatibilities

--- common/src/main/java/net/xstopho/resource_cracker/item/CrackHammerItem.java
+++ common/src/main/java/net/xstopho/resource_cracker/item/CrackHammerItem.java
@@ -2,7 +2,6 @@ package net.xstopho.resource_cracker.item;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
@@ -10,18 +9,13 @@ import net.xstopho.stophoslib.items.RecipeRemainder;
 
 import java.util.List;
 
-public class CrackHammerItem extends Item implements RecipeRemainder {
+public class CrackHammerItem extends RecipeRemainder.Item {
     public CrackHammerItem(int durability) {
-        super(new Properties().defaultDurability(durability));
+        super(new Properties().durability(durability));
     }
 
     @Override
-    public boolean hasCraftingRemainingItem() {
-        return true;
-    }
-
-    @Override
-    public ItemStack getRecipeRemainder(ItemStack stack) {
+    public ItemStack getRemainder(ItemStack stack) {
         if (stack.getDamageValue() < stack.getMaxDamage() - 1) {
             ItemStack moreDamaged = stack.copy();
             moreDamaged.setDamageValue(stack.getDamageValue() + 1);
