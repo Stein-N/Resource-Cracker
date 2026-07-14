f2bcb2554e97f2020bc78369802c8782b0777a21
Stein-N <nico-stein-98@sbvl.de>
2026-07-14 20:18:50 +0200

    fix forge

---
+++ forge/src/main/java/net/morthen/resource_cracker/ResourceCracker.java
@@ -0,0 +1,11 @@
+package net.morthen.resource_cracker;
+
+import net.minecraftforge.fml.common.Mod;
+import net.xstopho.resource_cracker.CrackerConstants;
+
+@Mod(CrackerConstants.MOD_ID)
+public class ResourceCracker {
+    public ResourceCracker() {
+        CrackerConstants.commonInit();
+    }
+}
