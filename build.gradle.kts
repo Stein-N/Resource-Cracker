6182fd63c0558eb8475fe5e5b404af641db504e9
Stein-N <nico-stein-98@sbvl.de>
2026-08-17 18:32:02 +0200

    update gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java")
     id("idea")
-    id("net.morthen.gradle.multiloader") version "0.1.9" apply false
+    id("net.morthen.gradle.multiloader") version "0.1.13" apply false
 
     id("net.fabricmc.fabric-loom") version "1.17-SNAPSHOT" apply false
     id("net.minecraftforge.gradle") version "[7.0.30, 8)" apply false
